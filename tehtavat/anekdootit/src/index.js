4cf8fad43ad3481cd832a0ee38cfaa78a66b5e5d
Petri Kallio <kallionpetri@gmail.com>
2018-08-29 16:15:25 +0300

    tehtävä 1.13

--- tehtavat/anekdootit/src/index.js
+++ tehtavat/anekdootit/src/index.js
@@ -1,38 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    const votesArray = new Array(anecdotes.length)
+    votesArray.fill(0, 0, anecdotes.length)
+
     this.state = {
-      anecdote: ''
+      anecdote: 0,
+      votes: votesArray
     }
   }
 
   getRandomAnecdote = () => () => {
       const randomIndex = Math.floor(Math.random() * this.props.anecdotes.length)
       
-      this.setState({anecdote: this.props.anecdotes[randomIndex]})
+      this.setState({anecdote: randomIndex})
+  }
+
+  voteCurrentAnecdote = () => () => {
+      const copyOfVotes = [...this.state.votes]
+      const anecdote = this.state.anecdote
+
+      copyOfVotes[anecdote] += 1
+
+      this.setState({ votes: copyOfVotes })
   }
 
   render() {
     return (
       <div>
-        <p>{this.state.anecdote}</p>
+        <p>{anecdotes[this.state.anecdote]}</p>
+        <p>has {this.state.votes[this.state.anecdote]} votes</p>
+        <button onClick={this.voteCurrentAnecdote()}>vote</button>
         <button onClick={this.getRandomAnecdote()}>next anecdote</button>
       </div>
     )
   }
 }
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
